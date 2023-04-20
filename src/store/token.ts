44d1ba5e7e3fa97523e44894a085c279b34be7a1
Micaiah Wallace <micaiah@snoapps.com>
2023-04-20 11:15:24 -0400

    fix token string type import

--- src/store/token.ts
+++ src/store/token.ts
@@ -1,12 +1,11 @@
 import { Instance, types } from 'mobx-state-tree'
-import { string } from 'mobx-state-tree/dist/internal'
 
 export const TokenModel = types.model('Token', {
   __type: types.optional(types.literal('token'), 'token'),
   id: types.identifier,
   family: types.string,
-  email: string,
-  access_token: string,
-  refresh_token: string,
+  email: types.string,
+  access_token: types.string,
+  refresh_token: types.string,
 })
 export type ITokenModel = Instance<typeof TokenModel>
